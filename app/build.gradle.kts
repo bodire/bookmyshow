fd915a370c3207b019b108d79db65db977e397fe
bodire <sandybodire8143@gmail.com>
2024-08-20 16:20:29 +0100

    added events and movies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.google.android.material:material:1.12.0")
 
     testImplementation("junit:junit:4.13.2")
 
---
+++ app/src/main/java/uk/ac/tees/mad/D3662700/Movies.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.mad.D3662700
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Movies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
+    }
+}
